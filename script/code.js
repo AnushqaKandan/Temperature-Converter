666faf287db3fc1ae93e84765cbb97186f5e760f
Name <anushqakandan03@gmail.com>
2024-05-20 10:14:14 +0200

    Edited

--- script/code.js
+++ script/code.js
@@ -20,18 +20,11 @@ fahrenheit.oninput = function () {
         fahrenheit.value) - 32) * 5 / 9 + 273.15;
     kelvin.value = parseFloat(k.toFixed(2));
 }
-kelvin.addEventListener('input', () => {
-    const tempKelvin = parseFloat(kelvin.value);
-    const tempFahrenheit = (tempKelvin - 273.15) * 9 / 5 + 32;
-    const tempCelsius = tempKelvin - 273.15;
-
-    fahrenheit.value = tempFahrenheit.toFixed(2);
-    celsius.value = tempCelsius.toFixed(2);
-});
 kelvin.oninput = function () {
     let f = (parseFloat(
         kelvin.value) - 273.15) * 9 / 5 + 32;
     fahrenheit.value = parseFloat(f.toFixed(2));
+
     let c = (parseFloat(kelvin.value) - 273.15);
     celsius.value = parseFloat(c.toFixed(2));
 }
